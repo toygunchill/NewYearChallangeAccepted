4462de459b101c81419ba27dd36804cf72a7708d
Toygun Çil <toyguncil1@gmail.com>
2024-01-02 23:35:38 +0300

    new project created

---
+++ ChallangeOne/ChallangeOne/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ChallangeOne
+//
+//  Created by Toygun Çil on 2.01.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ ChallangeOne/ChallangeOneUITests/ChallangeOneUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  ChallangeOneUITestsLaunchTests.swift
+//  ChallangeOneUITests
+//
+//  Created by Toygun Çil on 2.01.2024.
+//
+
+import XCTest
+
+final class ChallangeOneUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
